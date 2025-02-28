7cd97ca2b41baaae58dd82d4360e2955b118c9f8
schneiderleon <150476198+schneiderleon@users.noreply.github.com>
2025-02-28 22:10:18 +0100

    funktionalitäten und Gui angepasst
    fenster hinzugefügt
    funktionalitäten überarbeitet

--- Implementierung/SchwimmbadShop1/SchwimmbadShop1/obj/Debug/net7.0-windows/SchwimmbadShop1.AssemblyInfo.cs
+++ Implementierung/SchwimmbadShop1/SchwimmbadShop1/obj/Debug/net7.0-windows/SchwimmbadShop1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchwimmbadShop1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d0ea64097d681d4363ded9d8e759f58cd88ab8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchwimmbadShop1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchwimmbadShop1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Implementierung/SchwimmbadShop1/SchwimmbadShop1/obj/Debug/net7.0-windows/UserControl1.g.i.cs
+++ Implementierung/SchwimmbadShop1/SchwimmbadShop1/obj/Debug/net7.0-windows/UserControl1.g.i.cs
@@ -93,7 +93,7 @@ namespace SchwimmbadShop1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SchwimmbadShop1;component/usercontrol1.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SchwimmbadShop1;V1.0.0.0;component/usercontrol1.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\UserControl1.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
